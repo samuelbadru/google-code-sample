19af9176fecf0dab845129fbaed2386c65f76e70
Samuel Badru <samuelbadru@outlook.com>
2024-03-18 19:50:49 +0000

    Playing video of respective id

--- python/src/video_player.py
+++ python/src/video_player.py
@@ -34,7 +34,10 @@ class VideoPlayer:
         Args:
             video_id: The video_id to be played.
         """
-        print("play_video needs implementation")
+
+        video = self._video_library.get_video(video_id)
+
+        print(f"Playing video: {video.title}")
 
     def stop_video(self):
         """Stops the current video."""
