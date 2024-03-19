b1407e3b2dcd839a2f41b0c1a3d23c543180a5f1
Samuel Badru <samuelbadru@outlook.com>
2024-03-19 17:20:02 +0000

    playlist class initial implmenetation, to store playlist name

--- python/src/video_playlist.py
+++ python/src/video_playlist.py
@@ -3,3 +3,14 @@
 
 class Playlist:
     """A class used to represent a Playlist."""
+
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
